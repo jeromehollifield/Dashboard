d4b2a0fd4874eeecc9e88d0610d78781e01f3416
jeromehollifield <47490794+jeromehollifield@users.noreply.github.com>
2019-08-05 11:32:52 -0500

    Update NewUserConfirmationController.java

--- NewUserConfirmationController.java
+++ NewUserConfirmationController.java
@@ -24,7 +24,7 @@ import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
- * @author stevenhernandez
+ * @author User
  */
 public class NewUserConfirmationController implements Initializable {
 
